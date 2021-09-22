a5b39c8fbbfc6d4a014b036631d02350ed629dd0
Keira <KeiraJCoder@hotmail.com>
2021-09-22 14:45:54 +0100

    added

--- Playing/FizzBuzz.py
+++ Playing/FizzBuzz.py
@@ -66,6 +66,24 @@ if time >= (8):
 elif time >= (16): 
     print (place_of_home)
     
+# more complex  
+    
+place_of_work = "Codenation"
+town_of_home = "Manchester"
+
+check_time=input("Please enter the current time to find where I am?\n")
+check_time =int(check_time)
+if check_time < 8 or check_time > 18:
+        print(f"Antony is at{town_of_home}")
+elif check_time >= 8 and check_time < 9:
+        print("Antony is commuting")
+elif check_time >9 and check_time < 17:
+        print(f"Antony is at {place_of_work}")
+elif check_time >= 17 and check_time <18:
+        print("Antony is commuting")    
+else:
+    print("error, please try again")
+    
 # challenge 6
 
 num1 = 15
@@ -75,3 +93,16 @@ if (num1 + num2) % 2 == 0:
     print ("Success")
 else:
     print ("Massive Fail")
+
+
+#challenge 6 complex
+num1 = input("Please choose a number: \n")
+num1 = int(num1)
+num2 = input("Please choose another number: \n")
+num2 = int(num2)
+num3 = num1 + num2
+print(num3)
+if num3 % 2 == 0:
+    print("SUCCESS")
+else:
+    print("FAILURE")
