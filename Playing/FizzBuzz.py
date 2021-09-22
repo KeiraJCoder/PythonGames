6a1bbf86898b41bcfcc50dca800eb866486e4a70
Keira <KeiraJCoder@hotmail.com>
2021-09-22 14:28:54 +0100

    week1

--- Playing/FizzBuzz.py
+++ Playing/FizzBuzz.py
@@ -1,3 +1,20 @@
+#challenge 1
+
+password = input("Please choose a password: \n")
+
+if len(password) < 8:
+    print("ERROR... Password is too short... \nPassword must be at least 8 characters long. \n")
+else:
+    print(f"Your Password has been set to {password}")
+    
+ #challenge 2
+ 
+num = 15
+if num % 3 == 0 or num % 5 == 0:
+    print("This number is divisible by 3 or 5.")
+else:
+    print("This number is not divisible by 3 or 5.")   
+
 # challenge 3
 
 num = 126
@@ -11,7 +28,7 @@ elif num % 3 == 0:
 else:
     print (num)
 
-# stretch
+# challenge 3 stretch
 
 num = int(input("Enter a number: \n"))
 num = int(num)
@@ -28,3 +45,33 @@ elif num % 7 == 0:
 else:
     print(f"{num} is not fizz or buzz. you get no points")
 print(f"You Scored: {score}at the end. Well done!")
+
+#challenge 4
+word = input("Please select a word: \n")
+first_letter = word[0]
+last_letter = word[-1]
+if first_letter == last_letter:
+    print("The first and last letters are the same.")
+else:
+    print("The first and last letters do not match.")
+  
+#   challenge 5  
+
+time = 15
+town_of_work = ("chester")
+place_of_home = ("home")
+
+if time >= (8):
+    print (town_of_work)
+elif time >= (16): 
+    print (place_of_home)
+    
+# challenge 6
+
+num1 = 15
+num2 = 8
+
+if (num1 + num2) % 2 == 0:
+    print ("Success")
+else:
+    print ("Massive Fail")
